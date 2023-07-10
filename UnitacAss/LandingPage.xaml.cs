98ce1c670147f4610897baf34c0d61be3233cde9
Paul Maodi <paulmaodi2001@gmail.com>
2023-07-10 04:45:17 +0200

    LandingPage back-end complete.

---
+++ UnitacAss/LandingPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using UnitacAss.Models;
+using Newtonsoft.Json;
+
+namespace UnitacAss;
+
+public partial class LandingPage : ContentPage
+{
+	List<DayWeather> dayWeathers;
+	List<DayWeather> WeatherForcast
+	{
+		get { return dayWeathers; }
+		set {
+            dayWeathers = value;
+			OnPropertyChanged(nameof(WeatherForcast));
+		}
+	}
+	public LandingPage()
+	{
+		InitializeComponent();
+		DisplayForecast();
+
+    }
+
+    private async void DisplayForecast()
+    {
+        WeatherHandler weatherHandler = new WeatherHandler();
+		string weatherData = await weatherHandler.GetForecast();
+
+        
+        List<DayWeather> forecastItems = ParseForcastData(weatherData);
+
+       
+        WeatherForcast = forecastItems;
+    }
+
+    private List<DayWeather> ParseForcastData(string weatherData)
+    {
+        dynamic forecastJson = JsonConvert.DeserializeObject(weatherData);
+
+        List<DayWeather> forecastItems = new List<DayWeather>();
+
+        foreach (dynamic dailyForecast in forecastJson.forecast.forecastday)
+        {
+            string day = dailyForecast.date.ToString("dddd");
+            double minTemperature = Convert.ToDouble(dailyForecast.day.mintemp_c);
+            double maxTemperature = Convert.ToDouble(dailyForecast.day.maxtemp_c);
+
+            DayWeather forecastItem = new DayWeather { Day = day, MinTemp = minTemperature, MaxTemp = maxTemperature };
+
+            forecastItems.Add(forecastItem);
+        }
+
+        return forecastItems;
+    }
+
+    private void GotoDetailsPage(object sender, SelectedItemChangedEventArgs e)
+    {
+        if (e.SelectedItem is DayWeather selectedForecast)
+        {
+            
+            Navigation.PushAsync(new DetailsPage(selectedForecast));
+        }
+    }
+}
+
---
+++ UnitacAss/Models/DayWeather.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitacAss.Models
+{
+    internal class DayWeather
+    {
+        public string Day { get; set; }
+        public double MinTemp { get; set; }
+        public double MaxTemp { get; set; }
+    }
+}
