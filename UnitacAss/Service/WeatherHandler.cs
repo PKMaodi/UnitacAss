03e4540a9c7b9f2db130177ed5935e218cff4261
Paul Maodi <paulmaodi2001@gmail.com>
2023-07-12 18:07:51 +0200

    API successfuly integrated

--- UnitacAss/Service/WeatherHandler.cs
+++ UnitacAss/Service/WeatherHandler.cs
@@ -1,37 +1,38 @@
 ﻿using Newtonsoft.Json;
-
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
 namespace UnitacAss.Service
 {
     public class WeatherHandler
     {
-        private async void GetWeatherData(object sender, EventArgs e)
+        public async void GetWeatherData()
         {
-            // Make a request to the WeatherAPI.com API.
             var client = new HttpClient();
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Get,
-                RequestUri = new Uri("https://weatherapi-com.p.rapidapi.com/current.json?q=Pretoria"),
+                RequestUri = new Uri("https://weatherapi-com.p.rapidapi.com/forecast.json?q=Pretoria&days=7"),
                 Headers =
-                {
-                    { "X-RapidAPI-Key", "8d6a83b7a3mshe727e25aa80e7ebp1a6d73jsndbd2866866a7" },
-                    { "X-RapidAPI-Host", "weatherapi-com.p.rapidapi.com" },
-                },
+            {
+                { "X-RapidAPI-Key", "8d6a83b7a3mshe727e25aa80e7ebp1a6d73jsndbd2866866a7" },
+            { "X-RapidAPI-Host", "weatherapi-com.p.rapidapi.com" },
+            },
             };
-
             using (var response = await client.SendAsync(request))
             {
                 response.EnsureSuccessStatusCode();
                 var body = await response.Content.ReadAsStringAsync();
-
-                // Deserialize the JSON data into a CurrentWeather object.
-                var weather = JsonConvert.DeserializeObject<CurrentWeather>(body);
-
-                // Get the minimum and maximum temperatures.
-                var condition = weather.pretoriaWeather.condition;
-                var minTemp = weather.pretoriaWeather.min_temp;
-                var maxTemp = weather.pretoriaWeather.max_temp;
+                // Parse the JSON string
+                parseJSONData(body);
             }
+
+        }
+
+        private void parseJSONData(object body)
+        {
+            var forecastData = JsonConvert.DeserializeObject<ForecastData>(body);
         }
     }
 }
